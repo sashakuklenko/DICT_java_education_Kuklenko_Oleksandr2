abb760efc952b17f941501b6a248d8d484915b96
sashafrost <kuklenko.sasha@gmail.com>
2024-12-24 17:20:51 +0100

    Hangman 2-nd stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,4 +1,22 @@
 package Hangman;
+import java.util.Scanner;
 
 public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+
+        System.out.println("Guess the word:");
+
+        String wordToGuess = "java"; // Можете змінити це слово
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("> ");
+        String userGuess = scanner.nextLine();
+
+        if (userGuess.equals(wordToGuess)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
 }
