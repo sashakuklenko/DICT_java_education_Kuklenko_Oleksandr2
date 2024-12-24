b55d426cf0620efabf24c87c7f253635ddac8d89
sashafrost <kuklenko.sasha@gmail.com>
2024-12-24 17:24:34 +0100

    Hangman 4-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -6,21 +6,19 @@ public class hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
 
-
         String[] words = {"python", "java", "javascript", "kotlin"};
-
 
         Random random = new Random();
         String wordToGuess = words[random.nextInt(words.length)];
 
+        String hint = wordToGuess.substring(0, 2);
+        hint += "-".repeat(wordToGuess.length() - 2);
 
-        System.out.println("Guess the word:");
-
+        System.out.println("Guess the word " + hint + ":");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("> ");
         String userGuess = scanner.nextLine();
-
 
         if (userGuess.equals(wordToGuess)) {
             System.out.println("You survived!");
