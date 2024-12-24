79a7484fcd2d09a4fe20daa22dc54bd19acbd043
sashafrost <kuklenko.sasha@gmail.com>
2024-12-24 16:46:42 +0100

    ChatBot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,19 @@
+package ChatBot;
+
+import java.util.Scanner; // Підключення для роботи зі стандартним введенням
+
 public class ChatBot {
     public static void main(String[] args) {
-        String botName = "MyBot"; // Ім'я бота
-        int birthYear = 2024; // Рік створення
+        String botName = "MyBot";
+        int birthYear = 2024;
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+
+        System.out.println("Please, remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
     }
 }
