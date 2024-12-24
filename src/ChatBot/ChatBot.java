732fa7deeb1560ec53cdfd9a93405fba6d157ea1
sashafrost <kuklenko.sasha@gmail.com>
2024-12-24 16:42:48 +0100

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,9 @@
+public class ChatBot {
+    public static void main(String[] args) {
+        String botName = "MyBot"; // Ім'я бота
+        int birthYear = 2024; // Рік створення
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}
